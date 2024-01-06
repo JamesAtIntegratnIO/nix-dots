b24dac6e73362e1bc7c92177148f243d96cdfc12
jamesAtIntegratnIO <james@integratn.io>
2024-01-06 11:03:03 -0700

    fix: proxmox

--- hosts/lappy/hardware-configuration.nix
+++ hosts/lappy/hardware-configuration.nix
@@ -4,60 +4,68 @@
 {
   config,
   lib,
+  inputs,
   pkgs,
   modulesPath,
   ...
-}: {
+}:
+with lib; {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-  boot = {
-    kernelPackages = pkgs.linuxPackages_6_6;
-    kernelModules = ["kvm-intel"];
-    initrd = {
-      availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
-      kernelModules = [];
-    };
-    extraModulePackages = [];
-    # Bootloader.
-    loader = {
-      systemd-boot.enable = true;
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
+  config = mkMerge [
+    (mkIf (config.services.qemuGuest.enable == false) {
+      fileSystems."/" = {
+        device = "/dev/disk/by-label/NIXROOT";
+        fsType = "ext4";
       };
-    };
-    # Enable building for ARM
-    binfmt.emulatedSystems = ["aarch64-linux"];
-    # Setup keyfile
-    # initrd.secrets = {
-    #   "/crypto_keyfile.bin" = null;
-    # };
-    extraModprobeConfig = ''options bluetooth disable_ertm=1 snd slots=snd-hda-intel'';
-  };
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXROOT";
-    fsType = "ext4";
-  };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/NIXBOOT";
-    fsType = "vfat";
-  };
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-label/NIXBOOT";
+        fsType = "vfat";
+      };
 
-  swapDevices = [
+      swapDevices = [
+        {
+          device = "/.swapfile";
+        }
+      ];
+    })
     {
-      device = "/.swapfile";
+      boot = {
+        kernelPackages = pkgs.linuxPackages_6_6;
+        kernelModules = ["kvm-intel"];
+        initrd = {
+          availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
+          kernelModules = [];
+        };
+        extraModulePackages = [];
+        # Bootloader.
+        loader = {
+          systemd-boot.enable = true;
+          efi = {
+            canTouchEfiVariables = true;
+            efiSysMountPoint = "/boot";
+          };
+        };
+        # Enable building for ARM
+        binfmt.emulatedSystems = ["aarch64-linux"];
+        # Setup keyfile
+        # initrd.secrets = {
+        #   "/crypto_keyfile.bin" = null;
+        # };
+        extraModprobeConfig = ''options bluetooth disable_ertm=1 snd slots=snd-hda-intel'';
+      };
+
+      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+      # (the default) this is the recommended approach. When using systemd-networkd it's
+      # still possible to use this option, but it's recommended to use it in conjunction
+      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+      networking.useDHCP = lib.mkDefault true;
+      # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     }
   ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- proxmox.nix
+++ proxmox.nix
@@ -12,15 +12,21 @@ with lib; {
       proxmox.qemuConf = {
         name = config.modules.network.hostName;
         virtio0 = "local-zfs";
-        bios = "ovmf";
         cores = 4;
         memory = 4096;
         diskSize = "24096";
+        agent = true;
+        bios = "seabios";
       };
     }
   ];
+  # fileSystems."/".device = mkForce "/dev/disk/by-label/nixos";
 
-  fileSystems."/".device = mkForce "/dev/disk/by-label/nixos";
-  fileSystems."/boot".device = mkForce "/dev/disk/by-label/ESP";
-  # boot.loader.systemd-boot.enable = mkForce false;
+  boot.loader.systemd-boot.enable = mkForce false;
+
+  services = {
+    cloud-init.network.enable = true;
+    openssh.enable = true;
+    qemuGuest.enable = true;
+  };
 }
