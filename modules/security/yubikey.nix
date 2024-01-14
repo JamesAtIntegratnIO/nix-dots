76232f2a5c2ff0e8e465e1fe76ebd53b1b2c918d
jamesAtIntegratnIO <james@integratn.io>
2024-01-14 15:15:53 -0700

    add dependencies for yubikey management and setup yubikey pam

--- modules/security/yubikey.nix
+++ modules/security/yubikey.nix
@@ -17,9 +17,17 @@ in {
     }
     (mkIf cfg.yubikey.enable {
       security = {
-        pam.services = {
-          login.u2fAuth = true;
-          sudo.u2fAuth = true;
+        pam = {
+          services = {
+            login.u2fAuth = true;
+            sudo.u2fAuth = true;
+          };
+          yubico = {
+            enable = true;
+            debug = false;
+            mode = "challenge-response";
+            id = ["16411128"];
+          };
         };
         rtkit.enable = true;
       };
@@ -27,6 +35,38 @@ in {
         udev.packages = [pkgs.yubikey-personalization];
         pcscd.enable = true;
       };
+      environment.systemPackages = with pkgs; [
+        # Tools required
+        wget
+        # Tools for backing up keys
+        paperkey
+        pgpdump
+        parted
+        cryptsetup
+
+        # Yubico's official tools
+        yubikey-manager
+        yubikey-manager-qt
+        yubikey-personalization
+        yubikey-personalization-gui
+        yubico-piv-tool
+        yubioath-flutter
+
+        # Testing
+        ent
+
+        # Password generation tools
+        diceware
+        pwgen
+
+        # Miscellaneous tools that might be useful beyond the scope of the guide
+        cfssl
+        pcsctools
+
+        # For pinentry
+        pinentry
+        pinentry-rofi
+      ];
     })
   ];
 }
