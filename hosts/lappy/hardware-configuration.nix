4dde188e3f505eaaa2c78846a144c6bf88f518fd
jamesAtIntegratnIO <james@integratn.io>
2024-01-14 20:51:10 -0700

    reinstall with luks

--- hosts/lappy/hardware-configuration.nix
+++ hosts/lappy/hardware-configuration.nix
@@ -4,68 +4,61 @@
 {
   config,
   lib,
-  inputs,
   pkgs,
   modulesPath,
   ...
-}:
-with lib; {
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-  config = mkMerge [
-    (mkIf (config.services.qemuGuest.enable == false) {
-      fileSystems."/" = {
-        device = "/dev/disk/by-label/NIXROOT";
-        fsType = "ext4";
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
       };
-
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-label/NIXBOOT";
-        fsType = "vfat";
-      };
-
-      swapDevices = [
-        {
-          device = "/.swapfile";
-        }
-      ];
-    })
-    {
-      boot = {
-        kernelPackages = pkgs.linuxPackages_6_6;
-        kernelModules = ["kvm-intel"];
-        initrd = {
-          availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
-          kernelModules = [];
+    };
+    initrd = {
+      enable = true;
+      availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
+      kernelModules = ["dm-snapshot"];
+      luks.devices = {
+        luksroot = {
+          device = "/dev/disk/by-uuid/0fe72388-7f89-44f2-89c8-e50291c6a183";
+          preLVM = true;
         };
-        extraModulePackages = [];
-        # Bootloader.
-        loader = {
-          systemd-boot.enable = true;
-          efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot";
-          };
-        };
-        # Enable building for ARM
-        binfmt.emulatedSystems = ["aarch64-linux"];
-        # Setup keyfile
-        # initrd.secrets = {
-        #   "/crypto_keyfile.bin" = null;
-        # };
-        extraModprobeConfig = ''options bluetooth disable_ertm=1 snd slots=snd-hda-intel'';
       };
+    };
 
-      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-      # (the default) this is the recommended approach. When using systemd-networkd it's
-      # still possible to use this option, but it's recommended to use it in conjunction
-      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-      networking.useDHCP = lib.mkDefault true;
-      # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+    binfmt.emulatedSystems = ["aarch64-linux"];
+    extraModprobeConfig = ''
+      options bluetooth disable_ertm=1
+    '';
+  };
 
-      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    }
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/0e76f422-4250-4803-8916-6613ca2a2894";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/C245-E27F";
+    fsType = "vfat";
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/71a1a6f7-ed36-479f-b505-cf038d20a467";}
   ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
